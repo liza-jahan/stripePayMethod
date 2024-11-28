658e4558ff6ce6f937888c6efb290a7065d6b250
Eakteker jahan Liza <eaktekerjahan@gmail.com>
2024-11-28 22:01:42 +0600

    Update API

---
+++ src/main/java/com/example/stripepaymethod/model/CustomerData.java
@@ -0,0 +1,10 @@
+package com.example.stripepaymethod.model;
+
+import lombok.Data;
+
+@Data
+public class CustomerData {
+    private  String name;
+    private  String email;
+    private  String customerId;
+}
